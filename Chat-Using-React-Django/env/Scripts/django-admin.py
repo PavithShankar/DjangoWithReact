07b7c2b93bd7dc8a37070b436a5ab450a43ee648
PavithShankar <62866737+PavithShankar@users.noreply.github.com>
2020-04-20 18:47:28 +0530

    Django and ReactJS

---
+++ Chat-Using-React-Django/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Education Content\Work From Home\React Js\ProUp Project\Project Task\Chat-Using-React-Django\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
